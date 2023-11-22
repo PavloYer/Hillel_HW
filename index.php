e7c1f91f53eb71896ec0deb5d6ced4f7ec04aa63
Pavlo <pavlo.yerrr@gmail.com>
2023-11-22 09:37:58 +0200

    homework_6

--- index.php
+++ index.php
@@ -1,34 +1,17 @@
 <?php
 declare(strict_types=1);
 
-//Написати програму на PHP, яка містить користувацьку функцію для обчислення площі кола
-// та демонструє використання передачі даних у функцію за допомогою параметрів.
-function calcArea(int|float $area): int|float
+function calculate(int $value_1, int $value_2, ?closure $toPrint = null): int
 {
-    return pi() * pow($area, 2);
+    $var = $value_1 * $value_2;
+
+    if ($toPrint instanceof Closure) {
+        $toPrint($var);
+    }
+    return $var;
 }
 
-$area = 12.43;
-echo "Circle area: " . calcArea($area) . PHP_EOL;
-echo PHP_EOL;
+calculate(34, 345, function (int $value): void {
+    echo $value . PHP_EOL;
+});
 
-
-//Написати програму на PHP, яка приймає число і підносить його до ступеню:
-function getPower(int|float $number, int|float $power): int|float // коли вона повертає нове число
-{
-    return $number ** $power;
-}
-
-function getPowerAndChangeValue(int|float &$number, int|float $power): void // коли вона змінює передане число
-{
-    $number **= $power;
-}
-
-$userVal = 3.42;
-$power = 17.3;
-
-$result = getPower($userVal, $power);
-echo "New variable: $result" . PHP_EOL;
-
-getPowerAndChangeValue($userVal, $power) . PHP_EOL;
-echo "Changed default variable: $userVal" . PHP_EOL;
