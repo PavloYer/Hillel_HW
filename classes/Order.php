8771c3d870a9d551e37bf0e4b07010188b5ae24b
Pavlo <pavlo.yerrr@gmail.com>
2023-12-01 20:29:23 +0200

    Homework_8

--- classes/Order.php
+++ classes/Order.php
@@ -2,13 +2,20 @@
 
 namespace classes;
 
+use Exception;
+
 class Order
 {
     protected array $items = [];
 
     public function __construct(array $items)
     {
-        $this->items = $items;
+        $this->setItems($items);
+    }
+
+    private function setItems(array $items): void
+    {
+        $this->items = $this->validateOrder($items);
     }
 
     public function getItems(): array
@@ -21,14 +28,17 @@ class Order
         $total = 0;
 
         foreach ($this->items as $item) {
-            $total += $item[1] * $item[2];
+            $total += $item['amount'] * $item['price'];
         }
         return $total;
     }
 
     public function addToOrder(array $order): void
     {
-        $this->items[] = $order;
+        $order = $this->validateOrder($order);
+        foreach ($order as $item) {
+            $this->items[] = $item;
+        }
     }
 
     public function printOrder(): void
@@ -40,4 +50,29 @@ class Order
             echo PHP_EOL;
         }
     }
+
+    private function validateOrder(array $order): array
+    {
+        $validatedOrder = [];
+
+        foreach ($order as $item) {
+
+            extract($item);
+            if (empty($title) || !is_string($title)) {
+                throw new Exception('Title is invalid');
+            }
+
+            if (empty($amount) || !is_int($amount) || $amount < 1) {
+                throw new Exception('Amount is invalid');
+            }
+
+            if (empty($price) || !is_numeric($price) || $price <= 0) {
+                throw new Exception('Price is invalid');
+            }
+
+            $validatedOrder[] = ['title' => $title, 'amount' => $amount, 'price' => (float)$price];
+        }
+        return $validatedOrder;
+    }
+
 }
--- index.php
+++ index.php
@@ -6,18 +6,32 @@ use classes\Order;
 require_once 'configs.php';
 require_once ROOT . 'classes/Order.php';
 
-$quickOrder = new Order([
-    ['gloves', 300, 65.7],
-    ['boots', 42, 112],
-    ['coat', 7, 956],
-    ['hat', 34, 124.98],
-    ['shoes', 60, 375],
-]);
+
+try {
+    $quickOrder = new Order([
+        ['title' => 'gloves', 'amount' => 300, 'price' => 65.7],
+        ['title' => 'boots', 'amount' => 42, 'price' => 112],
+        ['title' => 'coat', 'amount' => 7, 'price' => 956],
+        ['title' => 'hat', 'amount' => 34, 'price' => 124.98],
+        ['title' => 'shoes', 'amount' => 60, 'price' => 375],
+    ]);
+} catch (Exception $exception) {
+    echo $exception->getMessage();
+    exit;
+}
 
 $quickOrder->printOrder() . PHP_EOL;
 echo "Total price: " . $quickOrder->getSum() . PHP_EOL;
 
-$quickOrder->addToOrder(['keyboard', 2, 1400]);
+try {
+    $quickOrder->addToOrder([
+        ['title' => 'keyboard', 'amount' => 2, 'price' => 1400]
+    ]);
+} catch (Exception $exception) {
+    echo $exception->getMessage();
+    exit;
+}
+
 $quickOrder->printOrder() . PHP_EOL;
 echo "Total price: " . $quickOrder->getSum() . PHP_EOL;
 
